ce1e53cd382226bf1a56d6068dd1e4e8a35a3080
Dafaathaillah <dafabintang76@gmail.com>
2021-06-22 16:16:34 +0800

    CRUD Kendaraan Masuk

---
+++ database/seeders/DeskripsiSistemSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DeskripsiSistemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
